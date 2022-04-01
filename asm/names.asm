93f783624bcc629c432cb16146b73a24b567329e
Gens81 <98492731+Gens81@users.noreply.github.com>
2022-04-01 09:37:32 +0200

    added names asm

---
+++ asm/names.asm
@@ -0,0 +1,88 @@
+;---------------------------------
+;    Actor names
+;---------------------------------
+
+org $C478C0
+
+	db "Terra "
+	db "Locke "
+	db "Cyan  "
+	db "Shadow"
+	db "Edgar "
+	db "Sabin "
+	db "Celes "
+	db "Strago"
+	db "Relm  "
+	db "Setzer"
+	db "Mog   "
+	db "Gau   "
+	db "Gogo  "
+	db "Umaro "
+	db "Banon "
+	db "Leo   "
+	db "Sombra"
+	db "Aeris "
+	db "Moglin"
+	db "Moglet"
+	db "Mogi  "
+	db "Molulu"
+	db "Mogan "
+	db "Moguel"
+	db "Mogara"
+	db "Mogpon"
+	db "Mugmug"
+	db "Cosmog"
+	db "Mog   "
+	db "????? "
+	db "Maduin"
+	db "????? "
+	db "Wedge "
+	db "Biggs "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "Kefka "
+	db "Kefka "
+	db "Kefka "
+	db "Kefka "
+	db "Kefka "
+	db "Kefka "
+	db "Kefka "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+	db "      "
+
+warnpc $C47A40
+
+;-----------------------------
+;     Naming screen chars
+;-----------------------------
+
+org $D8E8C8
+
+db $80,$81,$82,$83,$84,$9A,$9B,$9C,$9D,$9E ; A B C D E a b c d e
+db $85,$86,$87,$88,$89,$9F,$A0,$A1,$A2,$A3 ; F G H I J f g h i j
+db $8A,$8B,$8C,$8D,$8E,$A4,$A5,$A6,$A7,$A8 ; K L M N O k l m n o
+db $8F,$90,$91,$92,$93,$A9,$AA,$AB,$AC,$AD ; P Q R S T p q r s t
+db $94,$95,$96,$97,$98,$AE,$AF,$B0,$B1,$B2 ; U V W X Y u v w x y
+db $99,$B3,$D1,$C4,$C5,$C3,$D0,$C8,$C9,$CE ; Z z ñ - . á é í ó ú
+db $B4,$B5,$B6,$B7,$B8,$B9,$BA,$BB,$BC,$BD ; 0 1 2 3 4 5 6 7 8 9
+
+warnpc $D8E90E
