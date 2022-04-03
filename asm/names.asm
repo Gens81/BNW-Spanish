4ceac0d741d1df29ba529a2c305139d1db255c0e
Gens81 <98492731+Gens81@users.noreply.github.com>
2022-04-03 16:43:37 +0200

    Update names.asm
    switched position D0<>D1

--- asm/names.asm
+++ asm/names.asm
@@ -82,7 +82,7 @@ db $85,$86,$87,$88,$89,$9F,$A0,$A1,$A2,$A3 ; F G H I J f g h i j
 db $8A,$8B,$8C,$8D,$8E,$A4,$A5,$A6,$A7,$A8 ; K L M N O k l m n o
 db $8F,$90,$91,$92,$93,$A9,$AA,$AB,$AC,$AD ; P Q R S T p q r s t
 db $94,$95,$96,$97,$98,$AE,$AF,$B0,$B1,$B2 ; U V W X Y u v w x y
-db $99,$B3,$D1,$C4,$C5,$C3,$D0,$C8,$C9,$CE ; Z z ñ - . á é í ó ú
+db $99,$B3,$D0,$C4,$C5,$C3,$D1,$C8,$C9,$CE ; Z z ñ - . á é í ó ú
 db $B4,$B5,$B6,$B7,$B8,$B9,$BA,$BB,$BC,$BD ; 0 1 2 3 4 5 6 7 8 9
 
 warnpc $D8E90E
