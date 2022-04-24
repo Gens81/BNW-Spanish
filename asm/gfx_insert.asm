b373c4b2fe543db2813f8d7aa1db7644eeca7853
RyoNazzaHazuki <89749907+RyoNazzaHazuki@users.noreply.github.com>
2022-04-25 00:58:43 +0200

    Disabled Compressing data command & created new insert command via asm file

--- asm/gfx_insert.asm
+++ asm/gfx_insert.asm
@@ -14,3 +14,14 @@ org $C47FC0
 
 org $E96300
 	incbin "../gfx/modified/296300_The_End.bin"
+	
+;------------------------------------------------------------------
+;Cinematic&Title Program&GFX
+;------------------------------------------------------------------
+check bankcross off
+org $C2686C
+	incbin "../gfx/modified/C2686C_Cinematic_Progam.bin"	;Cinematic Program
+	
+org $D8f000
+	incbin "../gfx/modified/D8F000_Cinematich_Title_Isle_GFX.bin"	;Cinematic, Title, Isle GFX & Tilemap Properties
+check bankcross on
